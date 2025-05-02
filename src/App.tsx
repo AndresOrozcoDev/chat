7c21ef0d7e33fafcb50c2f5b2dbe67fdcbcc12be
Andres Orozco Dev <andres.orozco.dev@gmial.com>
2025-05-01 23:54:55 -0400

    add router and icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,37 @@
-import './App.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { useTheme } from './context/ThemeContext'
+import { Sun } from 'lucide-react';
+
+import Login from './features/auth/pages/Login'
+import Register from './features/auth/pages/Register'
+import Dashboard from './features/chat/pages/Dashboard'
+import NotFound from './utils/pages/NotFound'
+
+import './App.css'
+
 
 function App() {
   const { theme, toggleTheme } = useTheme()
 
   return (
-    <div className='bg-white dark:bg-gray-800 p-5'>
-      <p className='text-black dark:text-white'>Hello Chat!</p>
-      <button 
-        className='text-black dark:text-white' 
-        onClick={toggleTheme}
-      >
-        Cambiar a modo {theme === 'dark' ? 'claro' : 'oscuro'}
-      </button>
+    <div className='w-screen h-dvh relative bg-white text-black dark:bg-gray-100'>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+      <div className='bg-gray-200 rounded-full absolute p-3 right-2 bottom-2 dark:bg-white' title={theme === 'light' ? 'Modo oscuro' : 'Modo claro'}>
+        <Sun 
+          color="black" 
+          size={24} 
+          onClick={toggleTheme} 
+          className='cursor-pointer' 
+          aria-label={theme === 'light' ? 'Modo oscuro' : 'Modo claro'}
+        />
+      </div>
     </div>
   )
 }
---
+++ src/utils/pages/NotFound.tsx
@@ -0,0 +1,9 @@
+function NotFound() {
+  return (
+    <h1>
+      Hola
+    </h1>
+  );
+}
+
+export default NotFound;
