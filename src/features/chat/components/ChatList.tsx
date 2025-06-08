f34e6d934a9397ea4472231a733fc05138ff5c8b
Andres Orozco Dev <andres.orozco.dev@gmial.com>
2025-06-07 21:54:59 -0400

    add styles

--- src/features/chat/components/ChatList.tsx
+++ src/features/chat/components/ChatList.tsx
@@ -3,7 +3,7 @@ type ChatListProps = {};
 const ChatList = ({ }: ChatListProps) => {
 
   return (
-    <div className="h-full bg-white text-black border-r border-r-neutral-300 dark:bg-(--dark-bg-primary) dark:text-white dark:border-none">
+    <div className="h-full bg-white text-black px-3 py-5 border-r border-r-neutral-300 dark:bg-(--dark-bg-primary) dark:text-white dark:border-none">
       List
     </div>
   )
